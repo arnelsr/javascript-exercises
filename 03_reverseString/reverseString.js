d8229e8abcc2dfb6de1298a35f21dadc042cc342
arnelsr <arnel_villamin@yahoo.com>
2022-08-09 13:52:08 +0200

    Finish exercise 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,34 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    //split string
+    const stringArray = string.split(' ');
+    //get number of elements?
+    //loop thru all elements
+    let reversedWord ='';
+    let reversedWordArray = [];
+    let reversedStringsArray=[]
+    let word='';
+    for (let k=stringArray.length-1;k>-1;k--){
+      //get element from last to first
+      word=stringArray[k];
+   
+      //reverse each element
+        let j=0;
+        for (let i=word.length-1;i>-1;i--){
+            reversedWordArray[j] =word.charAt(i);
+            j++;
 
+        }
+        //convert reversedWordArray to string
+        reversedWord=reversedWordArray.join('');
+        //add to reverseWordArray
+        reversedStringsArray.push(reversedWord);
+      
+    }
+    //join each element with space between
+    //return
+    let reversedStrings='';
+    reversedStrings=reversedStringsArray.join(' ');
+    return reversedStrings;
 };
 
 // Do not edit below this line

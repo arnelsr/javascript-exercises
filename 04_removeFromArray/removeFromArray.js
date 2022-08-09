bddaa4ded502a7a66f98ba60fb2c7244e3c36029
arnelsr <arnel_villamin@yahoo.com>
2022-08-09 15:14:14 +0200

    Finish exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...deleteArray) {
+    //store array
+    
+    //remaining arguments should be deleted
+    //store list of values to be removed
+    //store rest of arguments as another array
+    
+   
+    
+    //declare new array to be returned
+    const returnedArray = [];
+    for (let i=0; i<array.length;i++){
+    //if element not in list to be deleted, store
+      if (!deleteArray.includes(array[i])) {
+        returnedArray.push(array[i]);
+
+
+      }
+  
+    }
+      //return array
+      return returnedArray;
 
 };
 

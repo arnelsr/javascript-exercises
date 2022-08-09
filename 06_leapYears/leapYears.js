beee2661bd28c19b4672ca5f36175b25fd8df82f
arnelsr <arnel_villamin@yahoo.com>
2022-08-09 16:32:57 +0200

    Finish exercise 06 leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let returnBoolean = true;
+    let remainder1 = 0;
+    let remainder2=0;
+    remainder1 = year%4
+    if (remainder1 !== 0) {
+       returnBoolean = false;
+       return returnBoolean;
+    }
+    remainder1 = year%100;
+    remainder2 = year%400;
+    if ((remainder1===0) &&
+        (remainder2 !==0)){
+        returnBoolean=false;
+        return returnBoolean;
+    }
+    return returnBoolean;
 };
 
 // Do not edit below this line

d92f61da2397bb0d90c662603ea92696bdd7a88c
arnelsr <arnel_villamin@yahoo.com>
2022-08-18 13:44:25 +0200

    Finish calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,58 @@
-const add = function() {
+const add = function(...args) {
+  const sum = args.reduce(function(total,number){
+   return total + number;
+    
+
+  },0);
+  return sum;
+};
+
+const subtract = function(...args) {
+  const difference = args.reduce(function(total,number){
+    return total - number;
+  
+
+	});
+  return difference;
+};
+
+const sum = function(array) {
+  const sum = array.reduce(function(total,number){
+    return total + number;
+     
+ 
+   },0);
+   return sum;
 	
 };
 
-const subtract = function() {
+const multiply = function(array) {
+  let product =1;
+  for (const number of array) {
+    product *= number;
+  }
+  return product;
+};
+
+const power = function(number1, number2) {
+  const powerResult=number1**number2;
+  return powerResult;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  if (number === 0 || number === 1) {
+    return result;
+  }
+  if (number < 0) {
+    result = -1;
+    return result;
+  }
+  for(let i=number;i>0;i--){
+    result *=i;
+  }
+	return result;
 };
 
 // Do not edit below this line

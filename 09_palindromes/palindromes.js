70ed65361235392023acdec0b7ed8d348ec87778
arnelsr <arnel_villamin@yahoo.com>
2022-08-19 14:51:56 +0200

    Finish palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,31 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    //convert to array, lowercase
+    const stringArray = Array.from(string.toLowerCase());
+    //filter a to z
+    const allLetters = stringArray.filter((char)=> {
+        return (/[a-zA-Z]/).test(char);
+    });
+    //reverse
+    let reverse=[];
+    
+    let i = allLetters.length - 1;
+    allLetters.forEach(letter =>{
+        reverse[i]=letter;
+        i--;
+        
+    });
+
+    //join orig array without spaces or punctuation
+    const originalString = allLetters.join('');
+    //join reverse array
+    const reverseString=reverse.join('');
+
+    //if orig and reverse equal return true
+    if (originalString === reverseString) {
+        return true;
+    }
+    return false;
 
 };
 

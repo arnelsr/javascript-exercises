860e19e432ed2cc2be689aa4401f20f3a3653851
arnelsr <arnel_villamin@yahoo.com>
2022-08-22 13:55:57 +0200

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(numberString) {
+    //convert argument to number
+    let number = +numberString;
+   
+
+    //no negatives
+    if ((number <= 0) || isNaN(number)){
+        return 'OOPS';
+    }
+   //return first 2 members
+    if (number === 1 || number === 2){
+        return 1;
+    }
+
+
+    let fibonacciMember = 0;
+    let fibonacciArray = [1,1];
+
+    for (let i=1; i<=number;i++) {
+        if (i>2) {
+            fibonacciMember = fibonacciArray[fibonacciArray.length-1] + fibonacciArray[fibonacciArray.length-2];
+            fibonacciArray.push(fibonacciMember);
+
+        }
+        
+    }
+    return fibonacciMember;
+
+    //calculate nth member
 
 };
 

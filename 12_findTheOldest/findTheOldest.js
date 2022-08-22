5d31575acd8f7ecc8ed3cdac02503eae2cc34352
arnelsr <arnel_villamin@yahoo.com>
2022-08-22 15:38:06 +0200

    Finish Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    //initial value age as 0
+    const initialAge = 0;
+    //current year
+    const day = new Date();
+    const year = day.getFullYear();
+    let oldest = [];
+    
+    
+    
+    
+    peopleArray.reduce((previousMaxAge,currentPerson)=>{
+        //current year if no year of death
+        let yearOfDeath = 0;
+        if (!currentPerson['yearOfDeath']) {
+            yearOfDeath= year;
+        }else{
+            yearOfDeath = currentPerson['yearOfDeath'];
+
+        }
+        
+        const currentMaxAge = yearOfDeath - currentPerson['yearOfBirth']
+        if (currentMaxAge>previousMaxAge) {
+            previousMaxAge=currentMaxAge;
+            oldest=currentPerson;
+
+        }
+        return previousMaxAge;
+
+    },initialAge);
+    return oldest;
 
 };
 
